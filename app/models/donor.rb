afcaf422d06ef9c4468e0bad4cb0491f02f7a939
Mathew Roberts <mathewroberts@mathews-air.ait-sydney.net.au>
2018-05-08 19:37:17 +1000

    devise confirmable

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -2,7 +2,7 @@ class Donor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_one :donor_profile
 end
