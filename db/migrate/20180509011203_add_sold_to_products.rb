dd2a75e0b6077d946f9b3cc7c6f8e607b4fe3f7a
Mathew Roberts <mathewroberts@mathews-air.ait-sydney.net.au>
2018-05-27 17:03:06 +1000

    added styling to home/login/profile

--- db/migrate/20180509011203_add_sold_to_products.rb
+++ db/migrate/20180509011203_add_sold_to_products.rb
@@ -1,5 +1,5 @@
 class AddSoldToProducts < ActiveRecord::Migration[5.2]
   def change
-    add_column :products, :sold, :boolean
+    add_column :products, :sold, :boolean, default: false
   end
 end
